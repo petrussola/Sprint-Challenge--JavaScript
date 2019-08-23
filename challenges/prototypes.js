4122941a1fe8b4219739cc09f70c1c466d31e226
Pere <pere.solaclaver@gmail.com>
2019-08-23 09:41:19 +0100

    Uncomment console logs to test the work, amended names of methods to make it work, fix sign in surfaceArea method

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -29,8 +29,8 @@ CuboidMaker.prototype.volume = function() {
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
-CuboidMaker.prototype.surface = function() {
-  return 2 * ( this.length * this.width + this.length *  this.height * this.width * this.height)
+CuboidMaker.prototype.surfaceArea = function() {
+  return 2 * ( this.length * this.width + this.length * this.height + this.width * this.height)
 }
 
 
@@ -39,10 +39,10 @@ CuboidMaker.prototype.surface = function() {
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
-let testCuboid = new CuboidMaker(4, 5, 5)
+let cuboid = new CuboidMaker(4, 5, 5)
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 
