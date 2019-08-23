5e9dbcd4e31bde3cc6ed275ba98bf1b33012ceda
Pere <pere.solaclaver@gmail.com>
2019-08-23 09:24:33 +0100

    Do step 2 functions challenge, create 3 functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,8 +7,8 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-function consume (param1, param2, cb) {
-  return cb(param1 * param2)
+function consume (num1, num2, cb) {
+  return cb(num1 * num2)
 }
 
 consume(2,4,console.log);
@@ -19,6 +19,16 @@ consume(2,4,console.log);
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+
+function add (num1, num2) {
+  return num1 + num2
+}
+function multiply (num1, num2) {
+  return num1 * num2
+}
+function greeting (firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
