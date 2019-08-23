b61b483f895ff42e8cdbd4d50257da0aa66f7611
Pere <pere.solaclaver@gmail.com>
2019-08-23 08:27:01 +0100

    Add roar method to Tyrannosaurus object

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -13,7 +13,10 @@ let tyrannosaurus = {
   diet: "carnivorous",
   weight: "7000kg",
   length: "12m",
-  period: "Late Cretaceous"
+  period: "Late Cretaceous",
+  roar: function() {
+    return `RAWERSRARARWERSARARARRRR!`
+  }
 }
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -51,7 +54,7 @@ console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
