de5f08532a85412c21c8d423f2aefd46570f87e0
Pere <pere.solaclaver@gmail.com>
2019-08-23 08:50:43 +0100

    Solve request 3 on creating array with universities that include 'uni' in their name

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -89,7 +89,7 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const uni = graduates.filter ( (item, i)=> graduates[i].university.includes("Uni"));
 console.log(uni);
 
 
